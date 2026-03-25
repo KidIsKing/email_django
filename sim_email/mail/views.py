d8e9fdec4a016d03388defbe24f784bacec89b6c
YanaMasalova <anulamasalova@gmail.com>
2026-03-25 17:27:47 +0500

    add first pagination

--- sim_email/mail/views.py
+++ sim_email/mail/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import Email
@@ -17,19 +18,28 @@ class SignUpView(CreateView):
 @login_required
 def inbox(request):
     emails = Email.objects.filter(folder='unread').order_by('-created_at')
-    return render(request, 'mail/inbox.html', {'emails': emails, 'active_tab': 'inbox'})
+    paginator = Paginator(emails, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'mail/inbox.html', {'page_obj': page_obj, 'active_tab': 'inbox'})
 
 
 @login_required
 def read_emails(request):
     emails = Email.objects.filter(folder='read').order_by('-created_at')
-    return render(request, 'mail/read.html', {'emails': emails, 'active_tab': 'read'})
+    paginator = Paginator(emails, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'mail/read.html', {'page_obj': page_obj, 'active_tab': 'read'})
 
 
 @login_required
 def sent_emails(request):
     emails = Email.objects.filter(folder='sent').order_by('-created_at')
-    return render(request, 'mail/sent.html', {'emails': emails, 'active_tab': 'sent'})
+    paginator = Paginator(emails, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'mail/sent.html', {'page_obj': page_obj, 'active_tab': 'sent'})
 
 
 @login_required
